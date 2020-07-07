39adfa88887611f73c503ea08409f197861a0531
yndira <yndinatera10@gmail.com>
2020-07-06 20:15:55 -0400

    avances

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,107 @@
-import { example, anotherExample } from '../src/data.js';
+import { defineFlag, sortedAscendent, sortedDescendent } from '../src/data.js';
 
 
-describe('example', () => {
+describe('defineFlag', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof defineFlag).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  const pais = 'CHI';
+  it('returns `defineFlag`', () => {
+    expect(defineFlag(pais)).toBe('CH');
+  });
+
+  const paisTwo = 'ITA';
+  it('returns for flagtwo `defineFlag`', () => {
+    expect(defineFlag(paisTwo)).toBe('IT');
   });
 });
 
 
-describe('anotherExample', () => {
+describe('sortAscendent', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof sortedAscendent).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  const athletes = [
+    {
+      nombre: 'Matteo Bisiani',
+    },
+    {
+      nombre: 'Carlos Alberto Pedroso Curiel',
+    },
+    {
+      nombre: 'Pascal Andr Jean Touron',
+    },
+    {
+      nombre: 'Aaron Wells Peirsol',
+    },
+    {
+      nombre: 'Aleksandr Aleksandrovich Karelin',
+    },
+  ];
+
+  it('returns `sortAscendent`', () => {
+    expect(sortedAscendent(athletes)).toStrictEqual([
+      {
+        nombre: 'Aaron Wells Peirsol',
+      },
+      {
+        nombre: 'Aleksandr Aleksandrovich Karelin',
+      },
+      {
+        nombre: 'Carlos Alberto Pedroso Curiel',
+      },
+      {
+        nombre: 'Matteo Bisiani',
+      },
+      {
+        nombre: 'Pascal Andr Jean Touron',
+      },
+    ]);
+  });
+});
+
+describe('sortDescendent', () => {
+  it('is a function', () => {
+    expect(typeof sortedDescendent).toBe('function');
+  });
+
+  const athletesTwo = [
+    {
+      nombre: 'Matteo Bisiani',
+    },
+    {
+      nombre: 'Carlos Alberto Pedroso Curiel',
+    },
+    {
+      nombre: 'Pascal Andr Jean Touron',
+    },
+    {
+      nombre: 'Aaron Wells Peirsol',
+    },
+    {
+      nombre: 'Aleksandr Aleksandrovich Karelin',
+    },
+  ];
+
+  it('returns `sortDescendent`', () => {
+    expect(sortedDescendent(athletesTwo)).toStrictEqual([
+      {
+        nombre: 'Pascal Andr Jean Touron',
+      },
+      {
+        nombre: 'Matteo Bisiani',
+      },
+      {
+        nombre: 'Carlos Alberto Pedroso Curiel',
+      },
+      {
+        nombre: 'Aleksandr Aleksandrovich Karelin',
+      },
+      {
+        nombre: 'Aaron Wells Peirsol',
+      },
+    ]);
   });
 });
